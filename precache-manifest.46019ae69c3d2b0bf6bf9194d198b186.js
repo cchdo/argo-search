a5eb6e5a5345fe3b549b7a614ddedd2c282ccfe2
Andrew Barna <abarna@gmail.com>
2020-02-26 12:38:03 -0800

    Updates

---
+++ precache-manifest.46019ae69c3d2b0bf6bf9194d198b186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4728c57d453ca8456651bfa7e71250c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fea42e825989250552f0",
+    "url": "./static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "49778fd4261ce29753ab",
+    "url": "./static/css/main.65f93353.chunk.css"
+  },
+  {
+    "revision": "fea42e825989250552f0",
+    "url": "./static/js/2.bdf495bf.chunk.js"
+  },
+  {
+    "revision": "9d2ac7f0bada33fee08aedb26c28c214",
+    "url": "./static/js/2.bdf495bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49778fd4261ce29753ab",
+    "url": "./static/js/main.ca0d5145.chunk.js"
+  },
+  {
+    "revision": "71458ffbf2db24ec0f89",
+    "url": "./static/js/runtime-main.5ae4402a.js"
+  }
+]);
