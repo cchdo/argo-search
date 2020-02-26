d344e5dd06a94cad236f1ce39d508c69f69557ba
Andrew Barna <abarna@gmail.com>
2020-02-26 14:28:25 -0800

    do a geo search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { Map, TileLayer } from 'react-leaflet';
 import { CircleMarker, Popup } from 'react-leaflet';
@@ -9,9 +9,9 @@ import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
 
 import { ApolloProvider } from "react-apollo";
-import { Query } from "react-apollo";
+import {useQuery} from '@apollo/react-hooks';
 
-import { FiHome, FiChevronRight, FiSearch, FiSettings } from "react-icons/fi";
+import { FiChevronRight, FiSearch} from "react-icons/fi";
 
 import L from 'leaflet';
 import marker from 'leaflet/dist/images/marker-icon.png';
@@ -43,16 +43,35 @@ const GET_LATEST_PROFILES = gql`
         }
 `;
 
+const GET_GEO_PROFILES = gql`
+  query GeoProfiles($geo: geography!) {
+    argo_profiles(order_by: {date: desc}, where: {_and: {geography: {_st_d_within: {from: $geo, distance:10000}}, date: {_is_null: false}}}) {
+      date
+      float_id
+      geography
+      file
+    }
+  }
+`;
+
 
 const ArgoTable = ({ loading, error, data }) => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error ...</p>;
 
-  return data.argo_profiles.map(({file,  float_id, date, geography }) => (
+
+  return (
+    <>
+    <h5>{data.argo_profiles.length} Profiles</h5>
+    <div>
+    {data.argo_profiles.map(({file,  float_id, date, geography }) => (
     <div key={file}>
-      <p>{float_id}: {date}</p>
+      <p>{float_id}: {date}<br /><small>{file}</small></p>
     </div>
-  ));
+  ))}
+  </div>
+  </>
+  )
 }
 
 const Markers = ({ loading, error, data }) => {
@@ -61,7 +80,7 @@ const Markers = ({ loading, error, data }) => {
   if (error) return <p>Error ...</p>;
 
   return data.argo_profiles.map(({file, float_id, geography }) => (
-    <CircleMarker key={file} radius={5} center={[geography.coordinates[1], geography.coordinates[0]]}>
+    <CircleMarker key={file} radius={4} center={[geography.coordinates[1], geography.coordinates[0]]}>
       <Popup>
         <h3>Argo Float: {float_id}</h3>
 
@@ -71,62 +90,50 @@ const Markers = ({ loading, error, data }) => {
   ));
 }
 
+function App() {
 
-class App extends Component {
+  const [collapsed, setCollapsed] = useState(false)
+  const [selected, setSelected] = useState('firstten');
+  const [search, setSearch] = useState(new URLSearchParams(window.location.search))
 
-  constructor(props){
-    super(props);
-    this.state = {
-      collapsed: false,
-      selected: 'firstten',
-      number: 10
-    }
+  const line = search.get("line")
+  let geo;
+  try {
+    geo = JSON.parse(line)
+  } catch (error) {
+    geo = {}
   }
 
-  onClose() {
-    this.setState({collapsed: true});
-  }
+  const {loading, error, data } = useQuery(GET_GEO_PROFILES, {
+    client: client,
+    variables: {geo: geo}
+  })
 
-  onOpen(id) {
-    this.setState({
-      collapsed: false,
-      selected: id,
-    })
-  }
-
-  render(){
-    return(
-      <ApolloProvider client={client}>
-        <Query query={GET_LATEST_PROFILES} variables={{number:this.state.number}}>
-            {(result) => {
-              return (
-                <div>
-                  <Sidebar
-                    id="sidebar"
-                    position="right"
-                    collapsed={this.state.collapsed}
-                    closeIcon={<FiChevronRight />}
-                      selected={this.state.selected}
-                      onOpen={this.onOpen.bind(this)}
-                      onClose={this.onClose.bind(this)}
-                    >
-                      <Tab id="firstten" header="First Fifty" icon={<FiSearch />}>
-                        <div>
-                          <ArgoTable {...result} />
-                        </div>
-                      </Tab>
-                    </Sidebar>
-                    <Map className="mapStyle" center={[0,0]} zoom={2}>
-                      <TileLayer attribute="" url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'} />
-                      <Markers {...result} />
-                    </Map>
-                  </div>
-              )}}
-                </Query>
-
-              </ApolloProvider>
-    )
-  }
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <Sidebar
+          id="sidebar"
+          position="right"
+          collapsed={collapsed}
+          closeIcon={<FiChevronRight />}
+          selected={selected}
+          onOpen={(id) => { setSelected(id); setCollapsed(false) }}
+          onClose={() => setCollapsed(true)}
+        >
+          <Tab id="firstten" header="Profile List" icon={<FiSearch />}>
+            <div>
+              <ArgoTable loading={loading} error={error} data={data} />
+            </div>
+          </Tab>
+        </Sidebar>
+        <Map className="mapStyle" center={[0, 0]} zoom={2}>
+          <TileLayer attribute="" url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+          <Markers loading={loading} error={error} data={data} />
+        </Map>
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App;
